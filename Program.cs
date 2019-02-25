660f190e4970ac5729647e8cc843d0b11d67306b
Lovet Nguatem <loveta1@109d25lx1s3f8j7.student.cssd.ab.ca>
2019-02-25 15:02:08 -0700

    finished all

--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace Luckysevens
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            int totalNumberOFSevens = 0;
+            Random rng = new Random();
+
+            int die1;
+            int die2;
+
+            for (int i = 1; i <= 100; i++){
+                die1 = rng.Next(1, 7);
+                die2 = rng.Next(1, 7);
+
+                if (die1 + die2 == 7)
+                totalNumberOFSevens++;
+            }
+            Console.WriteLine(" Out of 100 rolls, you rolled {0} sevens", totalNumberOFSevens);
+            Console.ReadLine();
     }
+}
 }
