24e4511cdfdb03b8d82bf52c5a0ecd46b58cf42e
Lovet Nguatem <loveta1@109d25lx1s3f8j7.student.cssd.ab.ca>
2019-02-25 14:17:40 -0700

    Luckysevens

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Luckysevens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
